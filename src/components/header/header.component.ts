0af21ec5bff8e5a2a83670e3ad8d646c80abe87a
ageng123 <ageng133@gmail.com>
2022-11-29 00:13:17 +0700

    2 files modified

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -5,11 +5,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
   userData = {
     firstName: 'Waseem',
-    lastName: 'Ahmad',
+    lastName: 'Akhmad',
   };
-  constructor() {}
-
   ngOnInit() {}
 }
